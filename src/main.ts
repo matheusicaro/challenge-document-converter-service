cb3665f07109dda3d47e1ea1dac7c4a283e12752
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-10 19:33:54 -0700

    Init a default project and add the first route

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('Example API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
