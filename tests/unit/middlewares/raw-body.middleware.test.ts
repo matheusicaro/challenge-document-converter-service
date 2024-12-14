034b0567a22b875fe1bbae3b92b1f583f2370a5e
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-14 14:22:20 -0700

    Added middleware and refactoring app to main

---
+++ tests/unit/middlewares/raw-body.middleware.test.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * TODO: Complete the unit tests in this ticket:
+ *
+ * @matheusicaro
+ **/
+describe("bodyParseToRawBody", () => {
+  it("should get the raw buffer correctly", async () => {});
+
+  it("should get the raw body to only to the expect content-type", async () => {});
+
+  it("should call next function when the parse is done", async () => {});
+
+  describe("when content-type is not accepted", () => {
+    it("should not change the initial body", async () => {});
+
+    it("should call next function", async () => {});
+  });
+});
