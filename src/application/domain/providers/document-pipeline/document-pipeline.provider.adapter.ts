9edeebe0ea0e90c30ecd6cae980b8c476bdcd66c
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-14 15:39:34 -0700

    Added initial pipeline logic

--- src/application/domain/providers/document-pipeline/document-pipeline.provider.adapter.ts
+++ src/application/domain/providers/document-pipeline/document-pipeline.provider.adapter.ts
@@ -1,4 +1,8 @@
-import { DocumentFormat } from "../../entities/document/document";
+import { DocumentFile, DocumentFormat } from "../../entities/document/document";
+import { Converter } from "../../../lib/converter";
+import { JsonDocumentConverterProviderAdapter } from "../document-converters/json-document-converter.provider.adapter";
+import { StringDocumentConverterProviderAdapter } from "../document-converters/string-document-converter.provider.adapter";
+import { XmlDocumentConverterProviderAdapter } from "../document-converters/xml-document-converter.provider.adapter";
 import {
   ConvertInput,
   ConvertResponse,
@@ -10,13 +14,35 @@ class DocumentPipelineProviderAdapter implements DocumentPipelineProviderPort {
     console.log(`instance of ${DocumentPipelineProviderAdapter.name}`);
   }
 
-  public convert(_input: ConvertInput): ConvertResponse {
+  public convert(input: ConvertInput): ConvertResponse {
+    const converter = this.getConverter(input.entryFile.newFormat);
+
+    converter.validate(input.entryFile);
+
+    const domainDocument = converter.consume(input.entryFile.content);
+
+    const newDocument = converter.convert(domainDocument);
+
     return {
-      document: {
-        content: "",
-        format: DocumentFormat.TEXT,
-      },
+      document: newDocument,
     };
+  }
+
+  private getConverter(documentFormat: DocumentFormat): Converter<DocumentFile> {
+    switch (documentFormat) {
+      case DocumentFormat.TEXT:
+        return new StringDocumentConverterProviderAdapter();
+
+      case DocumentFormat.XML:
+        return new XmlDocumentConverterProviderAdapter();
+
+      case DocumentFormat.JSON:
+        return new JsonDocumentConverterProviderAdapter();
+
+      default:
+        // TODO add a better error handler here
+        throw new Error("INVALID ARGUMENT");
+    }
   }
 }
 
