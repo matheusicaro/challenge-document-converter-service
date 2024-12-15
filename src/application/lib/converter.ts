d6542c99f14f82954d142b747412b586b1454c74
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-14 18:11:41 -0700

    Added common validation logic into the converter

--- src/application/lib/converter.ts
+++ src/application/lib/converter.ts
@@ -1,11 +1,29 @@
 import { DomainFile } from "../domain/entities/domain-file";
-import { DocumentFile } from "../domain/entities/document/document";
+import { DocumentFile, DocumentFormat } from "../domain/entities/document/document";
 import { EntryFile } from "../domain/entities/entry-file";
+import { InvalidArgumentError } from "matheusicaro-node-framework";
+import { isValidString } from "./string-helpers.lib";
 
 export abstract class Converter<DocumentConverted extends DocumentFile> {
-  public abstract validate(entryFile: EntryFile): void;
+  protected abstract documentFormat: DocumentFormat;
+
+  public validate(entryFile: EntryFile): void {
+    if (entryFile.newFormat !== this.documentFormat) {
+      throw new InvalidArgumentError(
+        `Invalid format received. This converter can only convert documents to ${this.documentFormat}.`,
+      );
+    }
+
+    if (!isValidString(entryFile.content)) {
+      throw new InvalidArgumentError("The entry file content is empty or invalid as a text");
+    }
+
+    this.internalValidation(entryFile);
+  }
+
+  protected abstract internalValidation(entryFile: EntryFile): void;
 
   public abstract consume(fileContentAsString: string): DomainFile;
 
-  public abstract convert(input: DomainFile): DocumentConverted;
+  public abstract convert(domainFile: DomainFile): DocumentConverted;
 }
---
+++ tests/unit/application/lib/converter.test.ts
@@ -0,0 +1,73 @@
+import { InvalidArgumentError } from "matheusicaro-node-framework";
+import { entryFileFactory } from "../../../factories/entry-file.factory";
+import { DocumentFormat } from "../../../../src/application/domain/entities/document/document";
+import { XmlDocument } from "../../../../src/application/domain/entities/document/xml-document";
+import { Converter } from "../../../../src/application/lib/converter";
+import { DomainFile } from "../../../../src/application/domain/entities/domain-file";
+import { EntryFile } from "../../../../src/application/domain/entities/entry-file";
+
+const mockedFunction = { runner: jest.fn() };
+
+class ExampleClass extends Converter<XmlDocument> {
+  protected documentFormat: DocumentFormat = DocumentFormat.XML;
+
+  protected internalValidation(entryFile: EntryFile): void {
+    mockedFunction.runner(entryFile);
+  }
+
+  public consume(fileContentAsString: string): DomainFile {
+    throw new Error("Method not implemented.");
+  }
+  public convert(input: DomainFile): XmlDocument {
+    throw new Error("Method not implemented.");
+  }
+}
+
+describe("Converter", () => {
+  const implementationClass = new ExampleClass();
+
+  const defaultValidEntryFile = entryFileFactory.build({
+    newFormat: DocumentFormat.XML,
+  });
+
+  describe("validate", () => {
+    /**
+     * This unit tests will guarantee that the internal validation method will run for who extends from Converter
+     *
+     * @matheusicaro
+     */
+    it("should call the abstract internalValidation when the first validation pass", () => {
+      implementationClass.validate(defaultValidEntryFile);
+
+      expect(mockedFunction.runner).toHaveBeenCalledTimes(1);
+      expect(mockedFunction.runner).toHaveBeenCalledWith(defaultValidEntryFile);
+    });
+
+    it("should throw error when a invalid entry file content is passed", () => {
+      const invalidDocument = {
+        ...defaultValidEntryFile,
+        content: "",
+      };
+
+      expect(() => implementationClass.validate(invalidDocument)).toThrow(
+        new InvalidArgumentError("The entry file content is empty or invalid as a text"),
+      );
+    });
+
+    describe("invalid new format", () => {
+      const invalidFormats = Object.values(DocumentFormat).filter(
+        (format) => format !== defaultValidEntryFile.newFormat,
+      );
+
+      it.each(invalidFormats)("should throw error when %s new format is passed", (format) => {
+        const invalidDocument = entryFileFactory.build({ newFormat: format });
+
+        expect(() => implementationClass.validate(invalidDocument)).toThrow(
+          new InvalidArgumentError(
+            "Invalid format received. This converter can only convert documents to XML.",
+          ),
+        );
+      });
+    });
+  });
+});
