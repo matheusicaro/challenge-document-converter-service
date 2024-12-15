a85f29a5f557d69cbebc53dc7d51232afe4cd064
Matheus Icaro <matheusicaro2@hotmail.com>
2024-12-15 04:04:47 -0700

    Bump libs and added todo tickets

--- src/configuration/dependency-registries/repositories-registries.ts
+++ src/configuration/dependency-registries/repositories-registries.ts
@@ -1,6 +1,15 @@
 import { Provider } from "@nestjs/common";
+import { DependencyRegistry } from "matheusicaro-node-framework";
+
 import { ProviderTokens } from "./tokens";
 import { DocumentPipelineProviderAdapter } from "../../application/domain/providers/document-pipeline/document-pipeline.provider.adapter";
+
+/**
+ * Starting default LoggerProvider DI in the container from @matheusicaro-node-framework
+ *
+ * ref: https://github.com/matheusicaro/matheusicaro-node-framework?tab=readme-ov-file#dependency-injection
+ **/
+const _dependencyRegistry = new DependencyRegistry([]);
 
 const registerProviders = (): Provider[] => {
   return [
--- tests/e2e/app/app.e2e-spec.ts
+++ tests/e2e/app/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import * as request from "supertest";
 import { AppModule } from "../../../src/configuration/app.module";
-import { entryDocumentFileFactory } from "../../factories/entry-document-file.factory";
 
 describe("E2E FileTest", () => {
   let app: INestApplication;
@@ -24,29 +23,66 @@ describe("E2E FileTest", () => {
   describe(URL, () => {
     const PATH = "/documents/converter/";
 
-    it("should convert text document to json document", async () => {
+    /**
+     *
+     * TODO: Finish the implementation for these unit in this ticket: https://github.com/matheusicaro/challenge-document-converter-service/issues/11#issue-2740212262
+     *
+     * @matheusicaro
+     **/
+    it.skip("should convert text document to json document", async () => {
       return request(app.getHttpServer())
         .post(PATH)
         .set("Content-Type", "text/plain")
         .set("new-format", "JSON")
         .set("segment-separator", "~")
         .set("element-separator", "*")
-        .send(entryDocumentFileFactory.buildStringDocument())
+        .send(
+          `
+            ProductID*4*8*15*16*23~
+            ProductID*a*b*c*d*e~
+            AddressID*42*108*3*14~
+            ContactID*59*26~
+        `,
+        )
         .expect(200)
         .expect({
-          body: entryDocumentFileFactory.buildJsonDocument(),
+          body: `
+              <root>
+                <ProductID>
+                    <ProductID1>4</ProductID1>
+                    <ProductID2>8</ProductID2>
+                    <ProductID3>15</ProductID3>
+                    <ProductID4>16</ProductID4>
+                    <ProductID5>23</ProductID5>
+                </ProductID>
+                <ProductID>
+                    <ProductID1>a</ProductID1>
+                    <ProductID2>b</ProductID2>
+                    <ProductID3>c</ProductID3>
+                    <ProductID4>d</ProductID4>
+                    <ProductID5>e</ProductID5>
+                </ProductID>
+                <AddressID>
+                    <AddressID1>42</AddressID1>
+                    <AddressID2>108</AddressID2>
+                    <AddressID3>3</AddressID3>
+                    <AddressID4>14</AddressID4>
+                </AddressID>
+                <ContactID>
+                    <ContactID1>59</ContactID1>
+                    <ContactID2>26</ContactID2>
+                </ContactID>
+              </root>
+            `,
         });
     });
 
-    it.skip("should response error when separators are not informed on processing string document", async () => {
-      return request(app.getHttpServer())
-        .post(PATH)
-        .set("Content-Type", "text/plain")
-        .set("new-format", "JSON")
-        .set("segment-separator", "")
-        .set("element-separator", "")
-        .send(entryDocumentFileFactory.buildStringDocument())
-        .expect(400);
+    it("should  when separators are not informed on processing string document", async () => {
+      expect("TODO").toEqual("TODO");
+    });
+
+    it("should return bad gateway and response correctly separators are not informed on processing string document", async () => {
+      expect("TODO").toEqual("TODO");
     });
   });
 });
